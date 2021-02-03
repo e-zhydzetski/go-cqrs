c6a563b5976471490a6abac0c6dbb9ded4065c5a
e.zhydzetski <e.zhydzetski@gmail.com>
2021-02-04 00:30:10 +0300

    test: improve concurrency test for event store

--- pkg/es/inmemes/load_test.go
+++ pkg/es/inmemes/load_test.go
@@ -4,60 +4,71 @@ import (
 	"context"
 	"github.com/e-zhydzetski/go-cqrs/pkg/es"
 	"gotest.tools/assert"
+	"math/rand"
 	"strconv"
+	"sync"
 	"testing"
+	"time"
 )
 
 func TestLoad(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	step1 := make(chan struct{})
-	step2 := make(chan struct{})
+	wg := sync.WaitGroup{}
 
 	store := New()
 
-	for k := 0; k < 10; k++ {
-		for i := 1; i <= 10; i++ {
+	publishNewEvent := func(streamID int) {
+		retry := 0
+		for {
+			events, _ := store.GetStreamEvents(ctx, "test_stream_"+strconv.Itoa(streamID))
+			var streamSeq uint32 = 0
+			if l := len(events); l > 0 {
+				streamSeq = events[l-1].Sequence
+			}
 			err := store.PublishEvents(ctx, &es.EventRecord{
-				Stream:   "test_stream_" + strconv.Itoa(k),
-				Sequence: uint32(i),
+				Stream:   "test_stream_" + strconv.Itoa(streamID),
+				Sequence: streamSeq + 1,
 				Type:     "test_event",
 				Data:     "test_data",
 			})
+			if err == es.ErrStreamConcurrentModification {
+				retry++
+				//fmt.Println("Retry", retry)
+				continue
+			}
 			assert.NilError(t, err)
+			break
 		}
-		events, _ := store.GetStreamEvents(ctx, "test_stream_"+strconv.Itoa(k))
-		assert.Equal(t, len(events), 10)
 	}
 
-	go func() {
-		counter := 0
-		_ = store.SubscribeOnEvents(ctx, es.FilterDefault().WithTypes("test_event"), func(event *es.EventRecord) bool {
-			counter++
-			if counter == 10 {
-				close(step1)
-			}
-			return counter < 200
-		})
-		close(step2)
-	}()
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 100; j++ {
+			go publishNewEvent(i)
+		}
+	}
 
-	for k := 10; k < 20; k++ {
+	for i := 0; i < 50; i++ {
+		wg.Add(1)
+		go func() {
+			time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+			counter := 0
+			_ = store.SubscribeOnEvents(ctx, es.FilterDefault().WithTypes("test_event"), func(event *es.EventRecord) bool {
+				counter++
+				return counter < 2000
+			})
+			wg.Done()
+		}()
+	}
+
+	for i := 0; i < 10; i++ {
 		go func(streamNumber int) {
-			<-step1
-			for i := 1; i <= 10; i++ {
-				err := store.PublishEvents(ctx, &es.EventRecord{
-					Stream:   "test_stream_" + strconv.Itoa(streamNumber),
-					Sequence: uint32(i),
-					Type:     "test_event",
-					Data:     "test_data",
-				})
-				assert.NilError(t, err)
+			for j := 0; j < 100; j++ {
+				time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+				go publishNewEvent(streamNumber)
 			}
-			events, _ := store.GetStreamEvents(ctx, "test_stream_"+strconv.Itoa(streamNumber))
-			assert.Equal(t, len(events), 10)
-		}(k)
+		}(i)
 	}
 
-	<-step2
+	wg.Wait()
 }
