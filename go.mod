d9b62af82d17c922c2efd482ac8dddb889b22c92
e.zhydzetski <e.zhydzetski@gmail.com>
2021-02-03 14:24:40 +0300

    feat: working on event store in process...

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/e-zhydzetski/go-cqrs
 go 1.15
 
 require (
+	github.com/e-zhydzetski/go-commons v0.0.0-20210123092159-834851148a7f
 	github.com/google/go-cmp v0.5.4 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	gotest.tools v2.2.0+incompatible
--- pkg/es/inmemes/store.go
+++ pkg/es/inmemes/store.go
@@ -2,6 +2,7 @@ package es
 
 import (
 	"context"
+	"github.com/e-zhydzetski/go-commons/pkg/xchan"
 	"github.com/e-zhydzetski/go-cqrs/pkg/es"
 	"sync"
 	"time"
@@ -9,6 +10,7 @@ import (
 
 func New() es.EventStore {
 	return &inMemoryEventStore{
+		seq:         es.StorePosBegin,
 		events:      NewList(),
 		streamIndex: map[string]*List{},
 	}
@@ -16,16 +18,19 @@ func New() es.EventStore {
 
 type inMemoryEventStore struct {
 	mx          sync.Mutex
+	seq         es.StorePosition
 	events      *List
 	streamIndex map[string]*List
-	// TODO subscribers
+
+	subsMx        sync.RWMutex
+	subscriptions []*xchan.Safe
 }
 
 func (s *inMemoryEventStore) PublishEvents(ctx context.Context, events ...*es.EventRecord) error {
 	if len(events) == 0 {
 		return nil // nothing to do
 	}
-	var stream string // should be the same for all events
+	var stream string // should be the same for all events, TODO maybe change API to pass stream once
 	for i, event := range events {
 		if event == nil {
 			return es.ErrInvalidEvent
@@ -39,36 +44,45 @@ func (s *inMemoryEventStore) PublishEvents(ctx context.Context, events ...*es.Ev
 		}
 	}
 
-	s.mx.Lock()
-	defer s.mx.Unlock()
+	if err := func() error {
+		s.mx.Lock()
+		defer s.mx.Unlock()
 
-	var prevSeq uint32 = 0
-	si, exists := s.streamIndex[stream]
-	if exists {
-		lastElement := si.GetLastElement()
-		if lastElement != nil {
-			prevSeq = lastElement.(*Node).Data.(*es.EventRecord).Sequence
+		// find/create index, detect current stream sequence
+		var prevSeq uint32 = 0
+		si, exists := s.streamIndex[stream]
+		if exists {
+			lastElement := si.GetLastElement()
+			if lastElement != nil {
+				prevSeq = lastElement.(*Node).Data.(*es.EventRecord).Sequence
+			}
+		} else {
+			si = NewList()
+			s.streamIndex[stream] = si
 		}
-	} else {
-		si = NewList()
-		s.streamIndex[stream] = si
-	}
 
-	for _, event := range events {
-		if prevSeq+1 != event.Sequence {
-			return es.ErrStreamConcurrentModification
+		// optimistic lock check
+		for _, event := range events {
+			if prevSeq+1 != event.Sequence {
+				return es.ErrStreamConcurrentModification
+			}
+			prevSeq = event.Sequence
 		}
-		event.Timestamp = time.Now()
-		prevSeq = event.Sequence
+
+		// set global sequence and store events
+		for _, event := range events {
+			s.seq++
+			event.GlobalSequence = s.seq
+			event.Timestamp = time.Now()
+			h := s.events.PushToHead(event) // update main list
+			si.PushToHead(h)                // update index
+		}
+		return nil
+	}(); err != nil {
+		return err
 	}
 
-	for _, event := range events {
-		h := s.events.PushToHead(event) // update main list
-		si.PushToHead(h)                // update index
-	}
-
-	// TODO maybe release mutex before
-	s.notifyAllAsync(events...)
+	s.notifyAllAsync(events)
 
 	return nil
 }
@@ -86,16 +100,83 @@ func (s *inMemoryEventStore) GetStreamEvents(ctx context.Context, stream string)
 	return events, nil
 }
 
-func (s *inMemoryEventStore) notifyAllAsync(events ...*es.EventRecord) {
-
+func checkFilter(filter es.EventFilter, event *es.EventRecord) bool {
+	if filter.Stream != "" && filter.Stream != event.Stream {
+		return false
+	}
+	if len(filter.Types) != 0 {
+		contains := false
+		for _, t := range filter.Types {
+			if t == event.Type {
+				contains = true
+				break
+			}
+		}
+		if !contains {
+			return false
+		}
+	}
+	if filter.Back {
+		return event.GlobalSequence <= filter.Pos
+	} else {
+		return event.GlobalSequence >= filter.Pos
+	}
 }
 
-func (s *inMemoryEventStore) SubscribeOnEvents(ctx context.Context, filter es.EventFilter) (<-chan *es.EventRecord, func(), error) {
-	if filter.Stream != "" {
-		// use stream index list
+func (s *inMemoryEventStore) notifyAllAsync(events []*es.EventRecord) {
+	s.subsMx.RLock()
+	defer s.subsMx.RUnlock()
+	for _, sub := range s.subscriptions {
+		func() {
+			for _, e := range events {
+				if !sub.Send(e) { // subscription closed, TODO remove closed subscriptions
+					return
+				}
+			}
+		}()
 	}
-	// TODO: read and publish all current events until head
-	// TODO: publish all new events by notifyAllAsync
+}
 
-	return nil, nil, nil // TODO: return safe channel, unsubscribe func, error
+func (s *inMemoryEventStore) SubscribeOnEvents(ctx context.Context, filter es.EventFilter, callbackFunc func(event *es.EventRecord)) error {
+	// iterate by history events
+	var err error
+	s.events.Iterate(func(data interface{}) bool {
+		err = ctx.Err()
+		if err != nil {
+			return false
+		}
+		e := data.(*es.EventRecord)
+		if checkFilter(filter, e) {
+			callbackFunc(e)
+		}
+		return true
+	}, filter.Back)
+	if err != nil {
+		return err
+	}
+
+	// subscribe on new events
+	if filter.Back {
+		return nil // no new events will be in the past
+	}
+	// TODO fix lost events saved after history read and before subscription create !
+	safeCh := xchan.MakeSafe()
+	defer safeCh.Close()
+	s.subsMx.Lock()
+	s.subscriptions = append(s.subscriptions, safeCh)
+	s.subsMx.Unlock()
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case raw, ok := <-safeCh.Ch():
+			if !ok {
+				return ctx.Err()
+			}
+			e := raw.(*es.EventRecord)
+			if checkFilter(filter, e) {
+				callbackFunc(e)
+			}
+		}
+	}
 }
